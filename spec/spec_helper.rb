4ec1c4a202f5e001418d9d6e4397a5839feee0bf
Robert Beekman <robert@matsimitsu.nl>
2012-08-03 15:22:02 +0200

    Final attempt to make travis ci play nice

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,12 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'active_support/all'
 require 'mongoid'
-require 'mongo'
 require 'uri'
 require 'mongoid-rspec'
 
+
 mongoid_config = YAML.load_file(File.join(File.dirname(__FILE__),"mongoid.yml"))['test']
+
 Mongoid.configure do |config|
   if !mongoid_config['uri'].blank?
     config.master = Mongo::Connection.from_uri(mongoid_config['uri']).db(mongoid_config['uri'].split('/').last)
@@ -17,7 +18,6 @@ Mongoid.configure do |config|
     config.master = Mongo::Connection.new(mongoid_config['host'], mongoid_config['port']).db(mongoid_config['database'])
   end
 end
-
 
 RSpec.configure do |config|
   config.mock_with :rspec
